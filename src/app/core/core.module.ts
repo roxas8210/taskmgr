168f541063d268fc698385df00d7c68e5de430f1
李华栈 <lihuazhan@lihuazhandeMacBook-Pro.local>
2017-09-13 23:45:40 +0800

    添加svg工具类

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,24 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdToolbarModule, MdIconModule } from '@angular/material';
+import { MdToolbarModule, MdIconModule, MdButtonModule, MdIconRegistry } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { loadSvgResources } from './utils/svg.utils';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     MdToolbarModule,
-    MdIconModule
+    MdIconModule,
+    MdButtonModule,
+    HttpModule
   ],
   declarations: [
     HeaderComponent,
@@ -26,9 +32,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ]
 })
 export class CoreModule {
-  constructor( @Optional() @SkipSelf() parent: CoreModule ) {
+  constructor( @Optional() @SkipSelf() parent: CoreModule, ir: MdIconRegistry, ds: DomSanitizer ) {
     if (parent) {
       throw new Error('模块已经存在，不能再次加载！');
     }
+    loadSvgResources(ir, ds);
   }
  }
